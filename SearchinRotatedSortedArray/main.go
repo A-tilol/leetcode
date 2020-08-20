ec117349485e80d3d5c6a26abb5c97356ebdd44e
atilol <ggrkssk@gmail.com>
2020-08-20 16:23:51 +0900

    new

---
+++ SearchinRotatedSortedArray/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	for i := range nums {
+		if nums[i] == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0))
+	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 3))
+	fmt.Println(search([]int{1}, 0))
+}
